71d15c81bc821a02df86d115ae16dbd595a095e2
pipupi <qiushic@andrew.cmu.edu>
2022-10-04 21:58:56 -0400

    test

--- solution-hw4/js/scripts.js
+++ solution-hw4/js/scripts.js
@@ -1,3 +1,5 @@
+
+
 //creating two arrays of glazing and packsize options for user to select
 
 let allGlazing = [
@@ -33,7 +35,7 @@ let allPacksize = [
 
 let glazingElement = document.querySelector('#glazingSelect');
 let packsizeElement = document.querySelector('#packsizeSelect');
-
+let addtocartElement = document.querySelector('#addtocart');
 //using loops to add options into the selected menus
 //value is set to be the index
 
@@ -75,8 +77,53 @@ function selectPacksize() {
 
 function updatePrice() {
     let detailTotal = document.querySelector('#detailTotal');
-    priceTotal = (2.49 + allGlazing[glazingInd].priceAdaptation) * allPacksize[packsizeInd].priceFactor;
+    priceTotal = (basePrice + allGlazing[glazingInd].priceAdaptation) * allPacksize[packsizeInd].priceFactor;
     detailTotal.innerText = '$' + priceTotal.toFixed(2);
+
+}
+
+//set up the roll class
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+//Create an empty array called cart
+const cart = [];
+
+//Parse the URL parameter and store the current roll type as a variable
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+//specified by ?roll=somthing in HTML hrefs
+const rollType = params.get('roll');
+
+let rollGlazing = allGlazing[glazingInd].glazing;
+let packSize = parseInt(allPacksize[packsizeInd].size);
+
+
+//Extract the current roll’s information
+let currentRoll = rolls[rollType];
+let basePrice = currentRoll['basePrice']
+
+function updateDetail(){
+    let imageSrc = currentRoll['imageFile'];
+    let header = document.querySelector('#productHeader');
+    let image = document.querySelector('#productImage');
+    let total = document.querySelector('#detailTotal');
+    
+    header.innerText = rollType + "Cinnamon Roll";
+    image.src = "./assets/products/" + imageSrc; 
+    total.innerText = "$" + currentRoll['basePrice'];
+
+}
+function updateCart(){
+    let newRoll = new Roll(rollType, rollGlazing, packSize, basePrice);
+    cart.push(newRoll);
+    console.log(JSON.stringify(cart));
 
 }
 
@@ -85,3 +132,9 @@ function updatePrice() {
 
 glazingElement.addEventListener('change', selectGlazing)
 packsizeElement.addEventListener('change', selectPacksize)
+addtocartElement.addEventListener('click', updateCart)  
+updateDetail();
+
+
+
+
